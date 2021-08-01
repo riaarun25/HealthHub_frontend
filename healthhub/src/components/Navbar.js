5bbabe988d069cf1aad128707e716700652c0ff1
Ria Arun <81374415+riaarun25@users.noreply.github.com>
2021-08-01 16:10:05 +0530

    Create Navbar.js

---
+++ healthhub/src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import Logo from "../assets/logofinal.png";
+import { Link } from 'react-router-dom';
+import ReorderIcon from "@material-ui/icons/Reorder";
+import "../styles/Navbar.css";
+
+function Navbar() {
+    const [openLinks, setOpenLinks] = useState(false)
+
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks);
+    };
+
+    return (
+        <div className="navbar">
+            <div className="leftSide" id={openLinks ? "open" : "close"}>
+                <img src={ Logo } />
+                <div className="hiddenLinks">
+                <Link to="/"> Home </Link>
+                <Link to="/ourvision"> Our Vision </Link>
+                <Link to="/healthhub"> HealthHub </Link>
+                <Link to="/docbot"> DocBot </Link>
+                <Link to="/contactus"> Contact Us </Link>
+                </div>
+            </div>
+            <div className="rightSide">
+                <Link to="/"> Home </Link>
+                <Link to="/ourvision"> Our Vision </Link>
+                <Link to="/healthhub"> HealthHub </Link>
+                <Link to="/docbot"> DocBot </Link>
+                <Link to="/contactus"> Contact Us </Link>
+                <button onClick={toggleNavbar}>
+                    <ReorderIcon />
+                </button>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
